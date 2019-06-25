d504f8ca16c9097393cafc9a9c2ffec7cabcff99
Eman Calso <egcalso@gmail.com>
2019-06-25 18:19:36 +0800

    Allow deletion and migration

--- setup.py
+++ setup.py
@@ -57,6 +57,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/bloodpet/epg_downloader',
-    version='0.3.4',
+    version='0.4.0',
     zip_safe=False,
 )
